07394a76d6f8e455bb5d300e34df5367fb248efc
Vamshi <chvamshikrishna8501@gmail.com>
2025-05-18 17:18:42 +0530

    updated resume

--- src/components/ResumeDownload.jsx
+++ src/components/ResumeDownload.jsx
@@ -12,7 +12,7 @@ const ResumeDownload = () => {
       >
         <h2 className="text-3xl font-semibold text-blue-400 mb-6">Download Resume</h2>
         <a
-          href="/resume/vamshichippa_resume.pdf"
+          href={process.env.PUBLIC_URL + "/resume/vamshichippa_resume.pdf"}
           download
           className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-medium py-3 px-6 rounded-full shadow-lg transition duration-300"
         >
